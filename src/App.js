3cd205a2003a80c199f57172565cad98f43ed887
bejker123 <1piotrkucharski@gmail.com>
2023-02-19 21:00:07 +0100

    tutorial backup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Square({value, onSquareClick}){
+  return <button
+          className="square"
+          onClick={onSquareClick}
+         >
+            {value}
+         </button>
+}
+
+function Row({values,onSquareClick}){
+  return <div className='board-row'>
+      <Square value={values[0]} onSquareClick={()=>{onSquareClick(values[0])}}/>
+      <Square value={values[1]} onSquareClick={()=>{onSquareClick(values[1])}}/>
+      <Square value={values[2]} onSquareClick={()=>{onSquareClick(values[2])}}/>
+  </div>
+}
+
+function Board() {
+  const [squares, setSquares] = useState([0,1,2,3,4,5,6,7,8]);
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row values={squares.slice(0,3)} onSquareClick={handleClick}/>
+      <Row values={squares.slice(3,6)} onSquareClick={handleClick}/>
+      <Row values={squares.slice(6,9)} onSquareClick={handleClick}/>
+    </>
   );
 }
 
-export default App;
+export default Board;
